e1f0bf774033bbc9525521d80a0056e899516618
Ahmed Ramadan <ahmednasser1902@gmail.com>
2025-08-14 13:51:03 +0300

    deploy setup v2

--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
